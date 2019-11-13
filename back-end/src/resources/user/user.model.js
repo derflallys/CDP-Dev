1e865cbda950cbcb4771a1ce12f93ff671398b70
ojellite <43299877+ojellite@users.noreply.github.com>
2019-11-13 19:54:05 +0100

    Regex e-mail

--- back-end/src/resources/user/user.model.js
+++ back-end/src/resources/user/user.model.js
@@ -36,4 +36,10 @@ UserSchema.pre('save', function(next) {
     next()
   })
 })
+
+UserSchema.path('email').validate(function(email) {
+  var emailRegex = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/
+  return emailRegex.test(email.text) // Assuming email has a text attribute
+}, 'The e-mail field cannot be empty.')
+
 export const User = mongoose.model('User', UserSchema)
