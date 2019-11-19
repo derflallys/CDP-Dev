8b7acc1c642dd787ad476e1ab69ee9fe32ea8dcf
Alfred Aboubacar SYLLA <a.aboubacar.sylla@gmail.com>
2019-11-20 00:53:39 +0100

    add var env

--- front-end/src/environments/environment.ts
+++ front-end/src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  BACK_END_URL : 'http://localhost:3000/api'
+  BACK_END_URL : 'http://localhost:3000/api',
+  BACK_END_URL_WITHOUT_API : 'http://localhost:3000'
 };
 
 /*
