17270da6c0eea017a5ab2b0313b851d8d613ed70
Alfred Aboubacar SYLLA <alfred-aboubacar.sylla@etu.u-bordeaux.fr>
2019-11-29 01:33:06 +0100

    fix import auth servie on home

--- front-end/src/app/components/home/home.component.ts
+++ front-end/src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {AuthenticationService} from '../../services/authentication.service';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,6 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
-  constructor() { }
+  constructor(private authService: AuthenticationService) { }
 
 }
